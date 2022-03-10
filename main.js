39a473db7a5b0f8c421c67a8e6049494f0c47c51
kakon4366 <kakonbarman4366@gmail.com>
2022-03-10 19:58:27 +0600

    Issue tracker fix all issue

--- main.js
+++ main.js
@@ -47,11 +47,11 @@ const fetchIssues = () => {
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3> ${ status === 'Closed' ? `<span class="bg-primary">${description}</span>` : description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              <a href="" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
 }
